bd949d8fc0265c8c34ae4ea351ab68a7ed733ad8
toluwase <toluwasethomas1@gmail.com>
2021-08-15 10:54:43 +0100

    save, add, update, os.stdIn

--- bill.go
+++ bill.go
@@ -29,6 +29,20 @@ fs:="Bill breakdown\n"
 		fs+=fmt.Sprintf("%-25v ...$%v \n", k+":", v)
 		total+=v
 	}
-	fs+=fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	//add tip
+	fs+=fmt.Sprintf("%-25v ...$%0.2f", "total:", b.tip)
+
+	//total
+	fs+=fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 	return fs
 }
+
+//update tip
+func (b *bill) updateTip(tip float64) {
+	b.tip=tip
+}
+// add tip
+func (b *bill) addItem(name string, price float64)   {
+	b.items[name]=price
+}
+
--- main.go
+++ main.go
@@ -1,8 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	mybill:= newBill("IceCream Bill")
-	fmt.Println(mybill.format())
+	mybill:= newBill("New Bill")
+	//mybill.updateTip(20)
+	//mybill.addItem("biscuit", 30)
+	promptOptions(mybill)
+	//fmt.Println(mybill.format())
+//createBill()
+
+}
+
+func createBill() bill  {
+	reader:=bufio.NewReader(os.Stdin)
+	//fmt.Print("Create a new bill name")
+	//name,_:=reader.ReadString('\n')
+	//name = strings.TrimSpace(name)
+	name,_ := getInput("Create a new bill name", reader)
+	b:= newBill(name)
+	fmt.Println("Created the bill", b.name)
+	return b
+}
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
+func promptOptions(b bill)  {
+	reader :=bufio.NewReader(os.Stdin)
+	opt,_ := getInput("Choose option: (a - add item, s - save, t-add tip): ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item Name: ", reader)
+		price,_ := getInput("Item Price: ", reader)
+		p,err:=strconv.ParseFloat(price,64)
+		if err != nil {
+			fmt.Print("The price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+		fmt.Println("Items added ", name, p)
+		promptOptions(b)
+	case "t":
+		tip, _:= getInput("Enter tip amount in $", reader)
+
+		t,err:=strconv.ParseFloat(tip,64)
+		if err != nil {
+			fmt.Print("The tip must be a number <3 ")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+		fmt.Println("Tip value updated: ", t)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("You saved the file: ", b.name)
+	default:
+		fmt.Println("Your option is invalid...")
+		promptOptions(b)
+	}
+}
+
+//save bill
+func (b *bill) save()  {
+	data:= [] byte (b.format()) //format and put into a byteslice, then save into data
+	err:= os.WriteFile("bills/"+b.name+".txt", data,0666)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Bill was saved to the file")
+
 }
